693413386bb13170c320aa8a9290b2b9a744d600
Ivan Chavero <ichavero@chavero.com.mx>
2017-03-02 16:34:31 -0700

    Add debugging stuff

--- build-router-commit.sh
+++ build-router-commit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export PERMISSIVE_GO=y 
 git commit -a --amend -m 'Add debugging stuff'
 hack/build-release.sh 
 hack/build-images.sh 
-docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:v3.5.0.1
+if [ $1 != "" ]; then
+  docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:$1
+fi
 docker stop $(docker ps | grep haproxy | cut -d ' ' -f 1)
 
--- build-router.sh
+++ build-router.sh
@@ -2,6 +2,8 @@
 
 hack/build-release.sh 
 hack/build-images.sh 
-docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:v3.5.0.1
+if [ $1 != "" ]; then
+  docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:$1
+fi
 docker stop $(docker ps | grep haproxy | cut -d ' ' -f 1)
 
