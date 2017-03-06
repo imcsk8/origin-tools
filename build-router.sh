8348e44ca328d0fdd6b65073772343037d2b7f4c
Ivan Chavero <ichavero@chavero.com.mx>
2017-03-05 21:39:10 -0700

    Add platform to build

--- build-router.sh
+++ build-router.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export OS_ONLY_BUILD_PLATFORMS="linux/amd64"
 hack/build-release.sh 
 hack/build-images.sh 
 if [ $1 != "" ]; then
   docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:$1
+  docker tag openshift/origin-haproxy-router:latest openshift/origin-haproxy-router:$1
 fi
 docker stop $(docker ps | grep haproxy | cut -d ' ' -f 1)
 
