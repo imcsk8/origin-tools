e406745d229ae017dfebacf2354b50ab4e534c1a
Ivan Chavero <ichavero@chavero.com.mx>
2017-03-29 14:40:38 -0700

    Add arch build option to router image script

--- build-router-image.sh
+++ build-router-image.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export OS_ONLY_BUILD_PLATFORMS="linux/amd64"
+
 OS_RELEASE=n hack/build-go.sh
 cp _output/local/bin/linux/amd64/openshift images/origin/bin/
 docker build -t openshift/origin images/origin
