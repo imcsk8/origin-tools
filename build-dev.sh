58c67ee1547d3f998605f1bfa9ce36e62aac83b9
Ivan Chavero <ichavero@chavero.com.mx>
2017-06-11 12:39:32 -0600

    Add dev image build script

---
+++ build-dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Building origin"
+export OS_ONLY_BUILD_PLATFORMS="linux/amd64"
+OS_RELEASE=n hack/build-go.sh
+
+echo "Building origin container"
+cp _output/local/bin/linux/amd64/openshift images/origin/bin/
+docker build -t openshift/origin images/origin
+
+echo "Deploying origin image"
+docker tag openshift/origin localhost:5000/openshift/origin
+docker push localhost:5000/openshift/origin
