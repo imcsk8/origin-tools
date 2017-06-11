d0c48ffd44ed106616b0753036c981a09cb67dd7
Ivan Chavero <ichavero@chavero.com.mx>
2017-06-11 12:51:47 -0600

    Add tag images script

---
+++ tag-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Need tag argument"
+  exit
+fi
+
+docker tag openshift/origin-pod:latest  openshift/origin-pod:$1
+docker tag openshift/origin-deployer:latest openshift/origin-deployer:$1
+docker tag openshift/origin-haproxy-router:latest  openshift/origin-haproxy-router:$1
+docker tag openshift/origin-haproxy-router:latest openshift3/ose-haproxy-router:$1
+
