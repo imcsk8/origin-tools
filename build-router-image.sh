f8db1a76b41fb71621d3cbdc2adf87ce8c65f307
Ivan Chavero <ichavero@redhat.com>
2017-07-25 15:49:32 -0600

    Change image building to the build-local-images.py script

--- build-router-image.sh
+++ build-router-image.sh
@@ -5,6 +5,5 @@ set -e
 export OS_ONLY_BUILD_PLATFORMS="linux/amd64"
 
 OS_RELEASE=n hack/build-go.sh
-cp _output/local/bin/linux/amd64/openshift images/origin/bin/
-docker build -t openshift/origin images/origin
-docker build -t openshift/origin-haproxy-router images/router/haproxy/
+hack/build-local-images.py openshift/origin
+hack/build-local-images.py openshift/origin-haproxy-router
