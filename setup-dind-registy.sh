dd1aaf7903bfb0e249392ec4cc13f81f7961f1f5
Ivan Chavero <ichavero@redhat.com>
2018-05-30 12:54:56 -0500

    Add no build images option

--- setup-dind-registy.sh
+++ setup-dind-registy.sh
@@ -6,11 +6,14 @@
 
 # Usage: cd origin; ../setup-dind-registy.sh  <image1> <image2> ... <imageN>
 
-IMAGES=$1
+IMAGES=""
 
 IFACE="enp0s25"
 
 DEFAULT_IMAGES="openshift/node openshift/origin-recycler openshift/origin-deployer openshift/origin-haproxy-router openshift/origin-nginx-router openshift/origin-template-service-broker openshift/origin-sti-builder openshift/origin-keepalived-ipfailover openshift/origin-f5-router openshift/origin-docker-builder openshift/origin-control-plane"
+
+dind_restart=false
+image_build=true
 
 function setup_node_registry {
     docker exec -t $1 sh -c "echo \"BLOCK_REGISTRY='--block-registry=all'\" >> /etc/sysconfig/docker"
@@ -35,27 +38,55 @@ function push_images {
     done
 }
 
+while getopts ":i:rn" opt; do
+  case ${opt} in
+    i )
+        echo "Building images: $OPTARG"
+        IMAGES=$OPTARG
+      ;;
+    r )
+        echo "Restart dind"
+        dind_restart=true
+      ;;
+   n)
+      echo "Images will not be built"
+      image_build=false
+      ;;
+    \? ) echo "Usage: cmd [-i images] [-s Restart dind]"
+         exit
+      ;;
+  esac
+done
 
 #cable export IP=`ip addr show enp0s25 |  grep -Po 'inet \K[\d.]+'`
 export IP=`ip addr show ${IFACE} |  grep -Po 'inet \K[\d.]+'` # wifi
-export TAG=`git describe --abbrev=0`
+export TAG=$(git describe --abbrev=0 2>&1)
 
 if [[ ${TAG} == *"fatal"* ]]; then
     echo "This script has to be run inside the openshift origin repo"
     exit
 fi
 
-hack/dind-cluster.sh stop
-hack/dind-cluster.sh start -rib
+status=$(docker ps -a --filter name=openshift-master --format {{.Status}})
+if [[ ${status} == *"Up"* ]] && [[ ${dind_restart} != true ]]; then
+    echo "Dind already up"
+else
+    echo "Starting dind"
+    hack/dind-cluster.sh stop
+    hack/dind-cluster.sh start -rib
+fi
 
-echo "Starting registry"
 
 status=$(docker ps -a --filter name=registry --format {{.Status}})
 
 if [[ ${status} == *"Exited"* ]]; then
+    echo "Restarting registry"
     docker start registry
-else
+elif [[ ${status} == "" ]]; then
+    echo "Starting registry"
     docker run -d -p 5000:5000 --restart=always --name registry registry:2
+else 
+    echo "Registry already running"
 fi
 
 if [[ ${IMAGES} == "" ]]; then
@@ -66,5 +97,8 @@ setup_node_registry openshift-master
 setup_node_registry openshift-node-1
 setup_node_registry openshift-node-2
 
-build_images
+if [[ ${image_build}} != false ]]; then
+    build_images
+fi
+
 push_images
