3430bc798d4e2796a488808fd47eeedfc7d3e2c4
Ivan Chavero <ichavero@redhat.com>
2018-08-01 01:12:59 -0500

    Fix user creation script

--- add_user.sh
+++ add_user.sh
@@ -2,9 +2,9 @@
 
 echo "Creating uaser: $1"
 echo \
-'{"kind":"ServiceAccount","apiVersion":"v1","metadata":{"name":"$1"}}' \
-| oc create -f -
+    "{\"kind\":\"ServiceAccount\",\"apiVersion\":\"v1\",\"metadata\":{\"name\":\"${1}\"}}" \
+    | oc create -f -
 
 echo "Add router user"
 
-oc patch scc privileged --type=json -p '[{"op": "add", "path": "/users/0", "value":"system:serviceaccount:default:$1"}]'
+oc patch scc privileged --type=json -p "[{\"op\": \"add\", \"path\": \"/users/0\", \"value\":\"system:serviceaccount:default:${1}\"}]"
