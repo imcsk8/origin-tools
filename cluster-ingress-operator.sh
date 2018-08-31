c10d6fd92218cc06d1c64a6032ca34786effc509
Ivan Chavero <ichavero@redhat.com>
2018-08-31 13:12:40 -0500

    Build image for cio

--- cluster-ingress-operator.sh
+++ cluster-ingress-operator.sh
@@ -5,11 +5,18 @@
 ORIGIN_PATH="${GOPATH}/src/github.com/openshift/origin"
 OC="${ORIGIN_PATH}/_output/local/bin/linux/amd64/oc"
 CIO_REPO="${GOPATH}/src/github.com/openshift/cluster-ingress-operator"
+IFACE="enp0s25"
+
+IP=`ip addr show ${IFACE} |  grep -Po 'inet \K[\d.]+'`
+
+cd ${CIO_REPO}
+
+operator-sdk build ${IP}:5000/openshift/cluster-ingress-operator
+docker push ${IP}:5000/openshift/cluster-ingress-operator
 
 cd ${ORIGIN_PATH}
 
 echo "Creating the Cluster Ingress Operator"
-${OC} create -f ${CIO_REPO}/deploy/cio-namespace.yaml
 ${OC} create -f ${CIO_REPO}/deploy/crd.yaml
 ${OC} create -f ${CIO_REPO}/deploy/rbac.yaml
 ${OC} create -f ${CIO_REPO}/deploy/operator.yaml
