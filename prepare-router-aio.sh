ba6ade2c9d0dd22659e74c1013da3845c0d2e88c
Ivan Chavero <ichavero@redhat.com>
2018-08-31 11:33:47 -0500

    Add prepare router script for all in one cluster

---
+++ prepare-router-aio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Creating router user"
+echo \
+    '{"kind":"ServiceAccount","apiVersion":"v1","metadata":{"name":"router"}}' \
+    | oc create -f -
+
+echo "Add router user"
+
+oc patch scc privileged --type=json -p '[{"op": "add", "path": "/users/0", "value":"system:serviceaccount:default:router"}]'
+
