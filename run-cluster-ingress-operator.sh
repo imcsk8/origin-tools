b6fc59aa06bfe140780c1afc9b2dd8ced2c570c6
Ivan Chavero <ichavero@redhat.com>
2018-10-22 14:29:03 -0600

    Add namespace option

--- run-cluster-ingress-operator.sh
+++ run-cluster-ingress-operator.sh
@@ -2,15 +2,22 @@
 
 # Run the cluster ingress operator
 
+
+NAMESPACE="default"
+
+if [[ $1 != "" ]]; then
+    NAMESPACE=$1
+fi
+
 #For a 4.0 cluster on aws created with the installer
-export KUBECONFIG="$HOME/Go/src/github.com/openshift/installer/auth/kubeconfig"
-export CIO_DIR="$HOME/Go/src/github.com/openshift/cluster-ingress-operator"
-export OPERATOR_NAME="$USERNAME-ingress-operator"
+export KUBECONFIG="${HOME}/Go/src/github.com/openshift/installer/auth/kubeconfig"
+export CIO_DIR="${HOME}/Go/src/github.com/openshift/cluster-ingress-operator"
+export OPERATOR_NAME="${USERNAME}-ingress-operator"
 
 echo "Preparing Cluster Ingress Operator"
 cd $CIO_DIR
 echo "Creating resources"
 oc apply -f manifests/ 
 echo "Starting Operator"
-operator-sdk up local namespace default --kubeconfig=$KUBECONFIG
+operator-sdk up local namespace ${NAMESPACE} --kubeconfig=${KUBECONFIG}
 
